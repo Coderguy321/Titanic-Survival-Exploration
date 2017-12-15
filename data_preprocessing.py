a2687e7970cac2df5d3eb3273377b80f81765cc5
Coderguy321 <vishwasgarg14@gmail.com>
2017-12-15 11:17:53 +0530

    step4 data completion done

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,5 +1,6 @@
 import sys
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 data_raw = pd.read_csv('train.csv')
 #data validation
@@ -77,8 +78,42 @@ data1['Title'] = data1['Title'].apply(lambda x: 'Misc' if title_names.loc[x] == 
 # data_val.info()
 # print(data1.sample(10))
 
+# print(data1.sample(1))
+#STEP 4 CONVERTING THE ENGLISH KEYWORDS TO MATHEMATICAL INPUTS
+label = LabelEncoder()
+for dataset in data_cleaner:
+    #this append it to the end of the row, so both exists
+    dataset['Embarked_Code'] = label.fit_transform(dataset['Embarked'])
+    dataset['Title_Code'] = label.fit_transform(dataset['Title'])
+    dataset['AgeBin_Code'] = label.fit_transform(dataset['AgeBin'])
+    dataset['FareBin_Code'] = label.fit_transform(dataset['FareBin'])
+    dataset['Sex_Code'] = label.fit_transform(dataset['Sex'])
+
+#define y variable aka target/outcome
+Target = ['Survived']
+#define x variables for original features aka feature selection
+data1_x = ['Sex','Pclass', 'Embarked', 'Title','SibSp', 'Parch', 'Age', 'Fare',
+           'FamilySize', 'IsAlone'] #pretty name/values for charts
+
+data1_x_calc = ['Sex_Code','Pclass', 'Embarked_Code', 'Title_Code','SibSp', 'Parch', 'Age', 'Fare'] #coded for algorithm calculation
+data1_xy = Target + data1_x
+
+#define x variables for original w/bin features to remove continuous variables
+data1_x_bin = ['Sex_Code','Pclass', 'Embarked_Code', 'Title_Code', 'FamilySize', 'AgeBin_Code', 'FareBin_Code']
+data1_xy_bin = Target + data1_x_bin
+# print('Bin X Y: ', data1_xy_bin, '\n')
+
+#define x and y variables for dummy features original
+data1_dummy = pd.get_dummies(data1[data1_x])
+# print(data1_dummy.sample(5))
+data1_x_dummy = data1_dummy.columns.tolist()
+print(data1_x_dummy)
+data1_xy_dummy = Target + data1_x_dummy
+# print('Dummy X Y: ', data1_xy_dummy, '\n')
 
 
+
+# print(data1_dummy.head())
 
 
 
