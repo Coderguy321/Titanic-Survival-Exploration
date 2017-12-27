8e39f9768679ce37c84f9ef859bcc0113c0d16cd
Coderguy321 <vishwasgarg14@gmail.com>
2017-12-27 13:32:20 +0530

    added different machine learning algorithms

--- model_data.py
+++ model_data.py
@@ -1,7 +1,48 @@
-from sklearn import ensemble
-
+from sklearn import ensemble, gaussian_process, linear_model, naive_bayes, neighbors, svm, tree, model_selection
+import pandas as pd
 MLA = [
+    #Ensemble Methods
     ensemble.AdaBoostClassifier(),
+    ensemble.BaggingClassifier(),
+    ensemble.ExtraTreesClassifier(),
+    ensemble.GradientBoostingClassifier(),
+    ensemble.RandomForestClassifier(n_estimators = 100),
 
+    #Gaussian Processes
+    gaussian_process.GaussianProcessClassifier(),
 
+    # GLM
+    linear_model.LogisticRegressionCV(),
+    linear_model.PassiveAggressiveClassifier(),
+    linear_model.RidgeClassifierCV(),
+    linear_model.SGDClassifier(),
+    linear_model.Perceptron(),
+
+    # Navies Bayes
+    naive_bayes.GaussianNB(),
+
+    # Nearest Neighbor
+    neighbors.KNeighborsClassifier(n_neighbors=3),
+
+    # SVM
+    svm.SVC(probability=True),
+    svm.LinearSVC(),
+
+    # Trees
+    tree.DecisionTreeClassifier(),
+    tree.ExtraTreeClassifier(),
 ]
+
+#Alternative to train_test_split
+cv_split = model_selection.ShuffleSplit(n_splits = 10, test_size = .3, train_size = .6, random_state = 0 )
+
+#Table to compare MLA
+MLA_columns = ['MLA Name', 'MLA Parameters','MLA Train Accuracy Mean', 'MLA Test Accuracy Mean',
+               'MLA Test Accuracy Min' ,'MLA Time']
+
+#creating dataframe with columns as specified below
+MLA_compare = pd.DataFrame(columns = MLA_columns)
+
+
+
+
