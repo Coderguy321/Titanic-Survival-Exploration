1c9322176692d0cae5ca7febeffe17bcd34c2945
Coderguy321 <vishwasgarg14@gmail.com>
2017-12-27 13:48:57 +0530

    plotted accuracy mean for diff algorithms

--- model_data.py
+++ model_data.py
@@ -1,5 +1,9 @@
 from sklearn import ensemble, gaussian_process, linear_model, naive_bayes, neighbors, svm, tree, model_selection
 import pandas as pd
+from data_preprocessing import data1, Target, data1_x_bin
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 MLA = [
     #Ensemble Methods
     ensemble.AdaBoostClassifier(),
@@ -43,6 +47,35 @@ MLA_columns = ['MLA Name', 'MLA Parameters','MLA Train Accuracy Mean', 'MLA Test
 #creating dataframe with columns as specified below
 MLA_compare = pd.DataFrame(columns = MLA_columns)
 
+# index through MLA and save performance to table
+row_index = 0
+for alg in MLA:
+    # set name and parameters
+    #https://stackoverflow.com/questions/36367736/use-name-as-attribute
+    MLA_compare.loc[row_index, 'MLA Name'] = alg.__class__.__name__
+    MLA_compare.loc[row_index, 'MLA Parameters'] = str(alg.get_params())
 
+    # score model with cross validation: http://scikit-learn.org/stable/modules/generated/sklearn.model_selection.cross_validate.html#sklearn.model_selection.cross_validate
+    cv_results = model_selection.cross_validate(alg, data1[data1_x_bin], data1[Target], cv=cv_split)
+
+    MLA_compare.loc[row_index, 'MLA Time'] = cv_results['fit_time'].mean()
+    MLA_compare.loc[row_index, 'MLA Train Accuracy Mean'] = cv_results['train_score'].mean()
+    MLA_compare.loc[row_index, 'MLA Test Accuracy Mean'] = cv_results['test_score'].mean()
+    MLA_compare.loc[row_index, 'MLA Test Accuracy Min'] = cv_results[
+        'test_score'].min()  # let's know the worst that can happen!
+
+    row_index += 1
+
+MLA_compare.sort_values(by = ['MLA Test Accuracy Mean'], ascending = False, inplace = True)
+# print(MLA_compare)
+
+#barplot using https://seaborn.pydata.org/generated/seaborn.barplot.html
+sns.barplot(x='MLA Test Accuracy Mean', y = 'MLA Name', data = MLA_compare, color = 'm')
+
+#prettify using pyplot: https://matplotlib.org/api/pyplot_api.html
+plt.title('Machine Learning Algorithm Accuracy Score \n')
+plt.xlabel('Accuracy Score (%)')
+plt.ylabel('Algorithm')
+plt.show()
 
 
